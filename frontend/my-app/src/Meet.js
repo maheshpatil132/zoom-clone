6ed320635283ca68984a06d5725777d81f9aef3d
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-08-09 18:14:00 +0530

    update-2

--- frontend/my-app/src/Meet.js
+++ frontend/my-app/src/Meet.js
@@ -29,8 +29,9 @@ const Meet = () => {
         const h1 = document.createElement('h1')
         h1.classList.add('text-3xl', 'text-center', 'absolute', 'capitalize')
         video.classList.add('w-80')
-        video.autoplay = true;
-        video.load();
+        video.addEventListener('loadedmetadata',()=>{
+            video.play()
+        })
         h1.textContent = name
         div.classList.add('border', 'rounded', 'p-1', 'bg-slate-400', 'overflow-hidden', 'relative', 'h-fit')
         div.appendChild(h1)
@@ -40,14 +41,37 @@ const Meet = () => {
 
     }
 
-    
+
     navigator.mediaDevices.getUserMedia({ video: true , audio:true}).then((strm) => {
         myvideoStrm = strm;
         if(myvideo.current){
             myvideo.current.srcObject = strm
         }
-    })
 
+        socket.on('user-connect', (id, size, username, parties) => {
+            // console.log(`new user : ${id}`);
+            call(id,username,strm)
+            socket.emit('tellname', name, id)
+        })
+
+        socket.on('addname', (name, id) => {
+            othername = name
+        })
+        
+        peer.on('call', (call) => {
+                call.answer(strm)
+                const video = document.createElement('video')
+                call.on('stream', (remote) => {
+                    append(video, remote, othername)
+                })
+                call.on('close', () => {
+                    video.remove()
+                    RemoveUnusedDivs()
+                })
+                answerList.push({ call })
+            })
+
+    })
 
 
     useEffect(() => {
@@ -58,28 +82,13 @@ const Meet = () => {
         })
 
         
-        peer.on('call', (call) => {
-            navigator.mediaDevices.getUserMedia({ video: true,audio:true }).then((strm) => {
-                call.answer(strm)
-                const video = document.createElement('video')
-                call.on('stream', async (remote) => {
-                    append(video, remote, othername)
-                })
-                call.on('close', () => {
-                    video.remove()
-                    RemoveUnusedDivs()
-                })
-                answerList.push({ call })
-            })
-        })
+     console.log("called");
 
     }, [])
 
-    socket.on('user-connect', (id, size, username, parties) => {
-        // console.log(`new user : ${id}`);
-        call(id,username,myvideoStrm)
-        socket.emit('tellname', name, id)
-    })
+   
+
+    
 
 
     socket.on('user-disconnected', (id) => {
@@ -97,9 +106,7 @@ const Meet = () => {
         }
     })
 
-    socket.on('addname', (name, id) => {
-        othername = name
-    })
+    
 
     const call = (id, username,strm) => {
             const call = peer.call(id, strm)
@@ -128,6 +135,7 @@ const Meet = () => {
     
     const VideoControl = ()=>{
        console.log(typeof(myvideoStrm))
+       console.log(myvideoStrm.getVideoTracks()[0].enabled)
         const enable = myvideoStrm.getVideoTracks()[0].enabled;
         if (enable) { // If Video on
             myvideoStrm.getVideoTracks()[0].enabled = false; // Turn off
@@ -136,14 +144,13 @@ const Meet = () => {
             // document.getElementById("video").style.color = "white"; // Change Color
             myvideoStrm.getVideoTracks()[0].enabled = true; // Turn On 
         }
+        
+        // myvideoStrm.getVideoTracks()[0].enabled = !(myvideoStrm.getVideoTracks()[0].enabled);
 
     }
 
     const startVideo = ()=>{
         console.log("start");
-        const enable = myvideoStrm.getVideoTracks()[0].enabled;
-            myvideoStrm.getVideoTracks()[0].enabled = true; // Turn On 
-            setVid(true)
     }
    
     const muteUnmute = () => { // Mute Audio
@@ -153,9 +160,10 @@ const Meet = () => {
         } else {
             myvideoStrm.getAudioTracks()[0].enabled = true; // UnMute
         }
+
     };
 
-
+   console.log(myvideoStrm);
 
     return (
         <div>
@@ -171,7 +179,7 @@ const Meet = () => {
                 <div className="footer flex fixed container bottom-0 w-full left-1/2 -translate-x-1/2 justify-center  rounded mx-auto gap-8 bg-neutral-400 p-3 ">
                     {
                         audio ?
-                            <div className='h-fit w-fit bg-slate-500 p-3 rounded-full'>
+                            <div onClick={muteUnmute} className='h-fit w-fit bg-slate-500 p-3 rounded-full'>
                                 <svg className="h-6 w-6   cursor-pointer text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />  <path d="M19 10v2a7 7 0 0 1-14 0v-2" />  <line x1="12" y1="19" x2="12" y2="23" />  <line x1="8" y1="23" x2="16" y2="23" /></svg>
                             </div>
                             :
@@ -190,7 +198,7 @@ const Meet = () => {
                                 <svg className=" h-6 w-6 cursor-pointer  text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M16 16v1a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h2m5.66 0H14a2 2 0 0 1 2 2v3.34l1 1L23 7v10" />  <line x1="1" y1="1" x2="23" y2="23" /></svg>
                             </div>
                     }
-                    <div onClick={muteUnmute} className=' flex justify-center p-3 rounded-full items-center bg-red-600'>
+                    <div  className=' flex justify-center p-3 rounded-full items-center bg-red-600'>
                         <svg className="h-7 w-7 cursor-pointer text-gray " width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
                     </div>
                 </div>
