242be589b702f33813089c5b70775d6a874fdf86
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-08-15 10:09:42 +0530

    video styling

--- backend/app.js
+++ backend/app.js
@@ -51,6 +51,7 @@ const PORT = process.env.PORT || 5000
        
       socket.to(room).emit("user-disconnected",id)
      })
+     
     
    })
     
--- frontend/my-app/src/Meet.js
+++ frontend/my-app/src/Meet.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Peer from 'peerjs'
 import io from 'socket.io-client'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+
 
 
 
@@ -18,8 +19,10 @@ const Meet = () => {
     const [media, setMedia] = useState(null)
     const mydiv = useRef()
     const myvideo = useRef()
-    const peer = new Peer()
 
+    const callRef = useRef()
+    const peer = new Peer()
+    const navigate = useNavigate()
 
     let othername = ' '
     var myvideoStrm;
@@ -31,7 +34,6 @@ const Meet = () => {
         const div = document.createElement('div')
         const h1 = document.createElement('h1')
         h1.classList.add('text-3xl', 'text-center', 'absolute', 'capitalize')
-        video.classList.add('w-80')
         video.addEventListener('loadedmetadata', () => {
             video.play()
         })
@@ -54,12 +56,12 @@ const Meet = () => {
             socket.emit('join', room, id, name)
         })
 
-        navigator.mediaDevices.getUserMedia({ video:{height:240},audio:true }).then((strm) => {
+        navigator.mediaDevices.getUserMedia({ video:{height:240,width:300},audio:true }).then((strm) => {
             myvideoStrm = strm
             setMedia(strm)
             if (myvideo.current) {
                 myvideo.current.srcObject =strm
-            }
+            } 
         })
         
     
@@ -75,6 +77,8 @@ const Meet = () => {
                 video.remove()
                 RemoveUnusedDivs()
             })
+
+            callRef.current = call
             answerList.push({ call })
         })
     })
@@ -126,6 +130,8 @@ const Meet = () => {
             video.remove()
             RemoveUnusedDivs()
         })
+
+        callRef.current = call
         callList.push({ id, call })
         
     }
@@ -160,7 +166,7 @@ const Meet = () => {
 
 
     const muteUnmute = () => { // Mute Audio
-        console.log(media);
+
         const enabled = media.getAudioTracks()[0].enabled; // Audio tracks are those tracks whose kind property is audio. Chck if array in empty or not
         if (enabled) { // If not Mute
             media.getAudioTracks()[0].enabled = false; // Mute
@@ -171,41 +177,41 @@ const Meet = () => {
         }
     };
 
+
+    const leave = ()=>{
+       console.log("disconnect")
+    //    socket.disconnect()
+    //    callRef.current.close()
+    //    navigate('/')
+    //    console.log(ids)
+    }
+
     return (
         <div>
-            <div className=' flex container p-8 mx-auto flex-col h-screen  bg-slate-300 '>
-                <h2 className='text-center text-green-600 text-3xl capitalize'>Voice chat</h2>
+            <div className=' flex p-8 mx-auto flex-col h-screen  bg-slate-300 '>
+                <h2 className='text-center text-green-600 text-3xl capitalize'>video chat</h2>
 
-                <div className=" p-2 box-container my-4 flex  flex-1 flex-wrap justify-center gap-4 mx-auto overflow-scroll" ref={mydiv} >
-                    <div className="left border rounded p-1 bg-slate-400 h-fit relative overflow-hidden " >
+                <div className=" p-2 box-container flex-1 my-4 flex flex-wrap  justify-center gap-4 mx-auto overflow-scroll " ref={mydiv} >
+                    <div className="left border h-fit rounded p-1 bg-slate-400 relative" >
                         <h1 className='text-3xl text-center absolute capitalize'>You</h1>
-                        <video muted={true} autoPlay={true} className='w-80' ref={myvideo}></video>
+                        <video muted={true} autoPlay={true}  ref={myvideo}></video>
                     </div>
+                    
                 </div>
 
-                <div className="footer flex  container  w-full justify-center  rounded mx-auto gap-8 bg-neutral-400 p-3 ">
-                    {
-                        audio ?
-                            <div onClick={muteUnmute} className='h-fit w-fit bg-slate-500 p-3 rounded-full'>
-                                <svg id='audio' className="h-6 w-6   cursor-pointer text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />  <path d="M19 10v2a7 7 0 0 1-14 0v-2" />  <line x1="12" y1="19" x2="12" y2="23" />  <line x1="8" y1="23" x2="16" y2="23" /></svg>
+                <div className="footer flex w-full justify-center rounded mx-auto gap-8 bg-neutral-400 p-3 ">
+                    
+                            <div  onClick={muteUnmute} className='h-fit w-fit bg-slate-500 p-3 rounded-full'>
+                                <svg  id='audio'  className="h-6 w-6  cursor-pointer text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <line x1="1" y1="1" x2="23" y2="23" />  <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />  <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />  <line x1="12" y1="19" x2="12" y2="23" />  <line x1="8" y1="23" x2="16" y2="23" /></svg>
                             </div>
-                            :
-                            <div className='h-fit w-fit bg-slate-500 p-3 rounded-full'>
-                                <svg className="h-6 w-6  cursor-pointer text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <line x1="1" y1="1" x2="23" y2="23" />  <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />  <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />  <line x1="12" y1="19" x2="12" y2="23" />  <line x1="8" y1="23" x2="16" y2="23" /></svg>
-                            </div>
-                    }
 
-                    {
-                        vid ?
-                            <div  onClick={VideoControl} className='bg-slate-500 p-3 rounded-full'>
-                                <svg id='video' className="h-6 w-6   cursor-pointer text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <polygon points="23 7 16 12 23 17 23 7" />  <rect x="1" y="5" width="15" height="14" rx="2" ry="2" /></svg>
+
+                   
+                            <div  onClick={VideoControl} className=' bg-slate-500 p-3 rounded-full'>
+                                <svg id='video' className=" h-6 w-6 cursor-pointer  text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M16 16v1a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h2m5.66 0H14a2 2 0 0 1 2 2v3.34l1 1L23 7v10" />  <line x1="1" y1="1" x2="23" y2="23" /></svg>
                             </div>
-                            :
-                            <div  className=' bg-slate-500 p-3 rounded-full'>
-                                <svg className=" h-6 w-6 cursor-pointer  text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M16 16v1a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h2m5.66 0H14a2 2 0 0 1 2 2v3.34l1 1L23 7v10" />  <line x1="1" y1="1" x2="23" y2="23" /></svg>
-                            </div>
-                    }
-                    <div className=' flex justify-center p-3 rounded-full items-center bg-red-600'>
+
+                    <div onClick={leave} className=' flex justify-center p-3 rounded-full items-center bg-red-600'>
                         <svg className="h-7 w-7 cursor-pointer text-gray " width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
                     </div>
                 </div>
